c18d6cd5e4825b6dfc19c1790b7ab2f9c6272e1f
Sebastian Łużyński <sebaluzik@interia.pl>
2017-05-29 11:45:33 +0200

    Update MyForm.h
    basic generated code by VS

--- Hellevator_2569e59dc85248b78994faae03635e04/MyForm.h
+++ Hellevator_2569e59dc85248b78994faae03635e04/MyForm.h
@@ -1,1 +1,59 @@
+#pragma once
 
+namespace TP_4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyść wszystkie używane zasoby.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+		/// zawartość tej metody z edytorem kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"MyForm";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
