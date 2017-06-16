da2540805546aaf9d13d178052db80b1df985732
Zekhire <s165173@student.pg.gda.pl>
2017-06-16 19:04:05 +0200

    Update Resource.h

--- PROJEKT4_1aea9531065f44bcaf2ade63ccf73678/TP_PROJECT4/TP_PROJECT4/Resource.h
+++ PROJEKT4_1aea9531065f44bcaf2ade63ccf73678/TP_PROJECT4/TP_PROJECT4/Resource.h
@@ -5,19 +5,46 @@
 
 #define IDS_APP_TITLE			103
 
-#define IDR_MAINFRAME			128
-#define IDD_TP_PROJECT4_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_TP_PROJECT4			107
-#define IDI_SMALL				108
-#define IDC_TP_PROJECT4			109
-#define IDC_MYICON				2
+#define IDR_MAINFRAME				128
+#define IDD_TP_PROJECT4_DIALOG		102
+#define IDD_ABOUTBOX			   	103
+#define IDM_ABOUT					    104
+#define IDM_EXIT					    105
+#define IDI_TP_PROJECT4				107
+#define IDI_SMALL				    	108
+#define IDC_TP_PROJECT4				109
+#define ID_BUTTON_5_4				201
+#define ID_BUTTON_5_3				202
+#define ID_BUTTON_5_2				203
+#define ID_BUTTON_5_1				204
+#define ID_BUTTON_4_5				205
+#define ID_BUTTON_4_3				206
+#define ID_BUTTON_4_2				207
+#define ID_BUTTON_4_1				208
+#define ID_BUTTON_3_5				209
+#define ID_BUTTON_3_4				210
+#define ID_BUTTON_3_2				211
+#define ID_BUTTON_3_1				212
+#define ID_BUTTON_2_5				213
+#define ID_BUTTON_2_4				214
+#define ID_BUTTON_2_3				215
+#define ID_BUTTON_2_1				216
+#define ID_BUTTON_1_5				217
+#define ID_BUTTON_1_4				218
+#define ID_BUTTON_1_3				219
+#define ID_BUTTON_1_2				220
+#define ID_START					500
+#define IDC_MYICON					2
+
+#define TMR_1						444
+#define TMR_2						445
+#define TMR_3						446
+#define TMR_4						447
+
 #ifndef IDC_STATIC
-#define IDC_STATIC				-1
+#define IDC_STATIC					-1
 #endif
-// Nastêpne wartoœci domyœlne dla nowych obiektów
+// NastÄ™pne wartoÅ›ci domyÅ›lne dla nowych obiektÃ³w
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
