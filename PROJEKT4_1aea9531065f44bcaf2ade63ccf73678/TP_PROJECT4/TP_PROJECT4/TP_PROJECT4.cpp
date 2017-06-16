580ebd37f7679aad99dbe94ef41445a6a7f81384
s165607 <s165607@student.pg.gda.pl>
2017-06-16 19:02:03 +0200

    Update TP_PROJECT4.cpp

--- PROJEKT4_1aea9531065f44bcaf2ade63ccf73678/TP_PROJECT4/TP_PROJECT4/TP_PROJECT4.cpp
+++ PROJEKT4_1aea9531065f44bcaf2ade63ccf73678/TP_PROJECT4/TP_PROJECT4/TP_PROJECT4.cpp
@@ -1,21 +1,97 @@
-// TP_PROJECT4.cpp: Okreœla punkt wejœcia dla aplikacji.
+// TP_PROJECT4.cpp: OkreÅ“la punkt wejÅ“cia dla aplikacji.
 //
 
 #include "stdafx.h"
 #include "TP_PROJECT4.h"
+#include "Resource.h"
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <vector>
+#include <windows.h>
+#include <time.h>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
 
 #define MAX_LOADSTRING 100
 
-// Zmienne globalne:
-HINSTANCE hInst;                                // bie¿¹ce wyst¹pienie
-WCHAR szTitle[MAX_LOADSTRING];                  // Tekst paska tytu³u
-WCHAR szWindowClass[MAX_LOADSTRING];            // nazwa klasy okna g³ównego
+HPEN Pen = CreatePen(PS_SOLID, 4, RGB(0, 0, 0));
+HPEN Pen2 = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
+class HUMAN
+{
+public:
+	int positionX;
+	int positionY;
+	int destination;
+	int source;
+	int weight;
+	int number;
+	void drawhuman(HDC hdc, int destination)
+	{
+		SelectObject(hdc, Pen);
+		Ellipse(hdc, positionX - 20, positionY - 45, positionX, positionY - 25); //gÅ‚owa
+		Rectangle(hdc, positionX - 30, positionY - 25, positionX + 10, positionY); //rÄ™ce
+		Rectangle(hdc, positionX - 20, positionY - 25, positionX, positionY); //brzuch
+		Rectangle(hdc, positionX - 20, positionY, positionX - 10, positionY + 25); //lewa noga
+		Rectangle(hdc, positionX - 10, positionY, positionX, positionY + 25); //prawa noga
+		SelectObject(hdc, Pen2);
+		switch (destination)
+		{
+		case 120:
+			MoveToEx(hdc, positionX - 3, positionY - 60, NULL);
+			LineTo(hdc, positionX - 3, positionY - 45);
+			MoveToEx(hdc, positionX - 17, positionY - 60, NULL);
+			LineTo(hdc, positionX - 17, positionY - 45);
+		case 336:
+			MoveToEx(hdc, positionX - 6, positionY - 60, NULL);
+			LineTo(hdc, positionX - 6, positionY - 45);
+			MoveToEx(hdc, positionX - 14, positionY - 60, NULL);
+			LineTo(hdc, positionX - 14, positionY - 45);
+		case 552:
+			MoveToEx(hdc, positionX - 10, positionY - 60, NULL);
+			LineTo(hdc, positionX - 10, positionY - 45);
+			break;
+		case 228:
+			MoveToEx(hdc, positionX - 3, positionY - 60, NULL);
+			LineTo(hdc, positionX - 3, positionY - 45);
+			MoveToEx(hdc, positionX - 17, positionY - 60, NULL);
+			LineTo(hdc, positionX - 17, positionY - 45);
+		case 444:
+			MoveToEx(hdc, positionX - 7, positionY - 60, NULL);
+			LineTo(hdc, positionX - 7, positionY - 45);
+			MoveToEx(hdc, positionX - 13, positionY - 60, NULL);
+			LineTo(hdc, positionX - 13, positionY - 45);
+			break;
+		}
+	}
+};
 
-// Przeka¿ dalej deklaracje funkcji do³¹czonych w tym module kodu:
+// Zmienne globalne:
+HINSTANCE hInst;                                // bieÂ¿Â¹ce wystÂ¹pienie
+WCHAR szTitle[MAX_LOADSTRING];                  // Tekst paska tytuÂ³u
+WCHAR szWindowClass[MAX_LOADSTRING];            // nazwa klasy okna gÂ³Ã³wnego
+
+// PrzekaÂ¿ dalej deklaracje funkcji doÂ³Â¹czonych w tym module kodu:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+void move(HWND hWnd, PAINTSTRUCT &ps, int a, int b)
+{
+	valueTimer1 = a;			//Å¼eby wpisaÄ‡ do windy polecenie zabrania ludzi z piÄ™tra, winda najpierw musi z niego odjechaÄ‡
+	if (controllerBlock)		//Blokada na wielokrotne wpisywanie polecenia wjazdu na to samo piÄ™tro
+	{
+		floors.push_back(a);
+		controllerBlock = FALSE;
+	}
+	floors.push_back(b);
+	repaintWindow(hWnd, hdc, ps);
+	if (timerblock)
+		SetTimer(hWnd, TMR_1, speed, 0);
+}
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -25,9 +101,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: W tym miejscu umieœæ kod.
+    // TODO: W tym miejscu umieÅ“Ã¦ kod.
 
-    // Zainicjuj ci¹gi globalne
+    // Zainicjuj ciÂ¹gi globalne
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_TP_PROJECT4, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
@@ -42,7 +118,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // G³ówna pêtla wiadomoœci:
+    // GÂ³Ã³wna pÃªtla wiadomoÅ“ci:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -60,7 +136,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 //  FUNKCJA: MyRegisterClass()
 //
-//  CEL: Rejestruje klasê okna.
+//  CEL: Rejestruje klasÃª okna.
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
@@ -86,20 +162,43 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 //   FUNKCJA: InitInstance(HINSTANCE, int)
 //
-//   CEL: Zapisuje dojœcie wyst¹pienia i tworzy okno g³ówne
+//   CEL: Zapisuje dojÅ“cie wystÂ¹pienia i tworzy okno gÂ³Ã³wne
 //
 //   KOMENTARZE:
 //
-//        W tej funkcji dojœcie wyst¹pienia jest zapisywane w zmiennej globalnej i
-//        jest tworzone i wyœwietlane okno g³ówne programu.
+//        W tej funkcji dojÅ“cie wystÂ¹pienia jest zapisywane w zmiennej globalnej i
+//        jest tworzone i wyÅ“wietlane okno gÂ³Ã³wne programu.
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Przechowuj dojœcie wyst¹pienia w zmiennej globalnej
+   hInst = hInstance; // Przechowuj dojÅ“cie wystÂ¹pienia w zmiennej globalnej
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
+  hwndButton = CreateWindow(TEXT("button"), TEXT("4"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 40, 20, 20, hWnd, (HMENU)ID_BUTTON_5_4, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("3"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 60, 20, 20, hWnd, (HMENU)ID_BUTTON_5_3, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("2"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 80, 20, 20, hWnd, (HMENU)ID_BUTTON_5_2, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("1"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 100, 20, 20, hWnd, (HMENU)ID_BUTTON_5_1, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("5"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 148, 20, 20, hWnd, (HMENU)ID_BUTTON_4_5, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("3"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 168, 20, 20, hWnd, (HMENU)ID_BUTTON_4_3, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("2"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 188, 20, 20, hWnd, (HMENU)ID_BUTTON_4_2, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("1"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 208, 20, 20, hWnd, (HMENU)ID_BUTTON_4_1, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("5"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 256, 20, 20, hWnd, (HMENU)ID_BUTTON_3_5, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("4"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 276, 20, 20, hWnd, (HMENU)ID_BUTTON_3_4, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("2"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 296, 20, 20, hWnd, (HMENU)ID_BUTTON_3_2, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("1"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 316, 20, 20, hWnd, (HMENU)ID_BUTTON_3_1, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("5"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 364, 20, 20, hWnd, (HMENU)ID_BUTTON_2_5, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("4"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 384, 20, 20, hWnd, (HMENU)ID_BUTTON_2_4, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("3"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 404, 20, 20, hWnd, (HMENU)ID_BUTTON_2_3, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("1"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle + 574, 424, 20, 20, hWnd, (HMENU)ID_BUTTON_2_1, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("5"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 472, 20, 20, hWnd, (HMENU)ID_BUTTON_1_5, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("4"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 492, 20, 20, hWnd, (HMENU)ID_BUTTON_1_4, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("3"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 512, 20, 20, hWnd, (HMENU)ID_BUTTON_1_3, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("2"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, middle - 594, 532, 20, 20, hWnd, (HMENU)ID_BUTTON_1_2, hInstance, NULL);
+	hwndButton = CreateWindow(TEXT("button"), TEXT("START"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 1000, 0, 50, 50, hWnd, (HMENU)ID_START, hInstance, NULL);
+  
+  
    if (!hWnd)
    {
       return FALSE;
@@ -114,11 +213,11 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 //  FUNKCJA: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
-//  CEL: Przetwarza wiadomoœci dla okna g³ównego.
+//  CEL: Przetwarza wiadomoÅ“ci dla okna gÂ³Ã³wnego.
 //
-//  WM_COMMAND — przetwarzaj menu aplikacji
-//  WM_PAINT — pomaluj okno g³ówne
-//  WM_DESTROY — opublikuj komunikat o rezygnacji i wróæ
+//  WM_COMMAND â€” przetwarzaj menu aplikacji
+//  WM_PAINT â€” pomaluj okno gÂ³Ã³wne
+//  WM_DESTROY â€” opublikuj komunikat o rezygnacji i wrÃ³Ã¦
 //
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -146,7 +245,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: tutaj dodaj kod rysowania u¿ywaj¹cy elementu hdc...
+            // TODO: tutaj dodaj kod rysowania uÂ¿ywajÂ¹cy elementu hdc...
             EndPaint(hWnd, &ps);
         }
         break;
@@ -159,7 +258,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-//Procedura obs³ugi wiadomoœci dla okna informacji o programie.
+//Procedura obsÂ³ugi wiadomoÅ“ci dla okna informacji o programie.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
