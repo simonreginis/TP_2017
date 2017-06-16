3364bc69c649c216217f0bb17a0fa4e3f9849e4a
SP2FET <sp2fet@gmail.com>
2017-06-16 20:29:17 +0200

    Added simple MessageBox for test

--- TP_4003ec51a8b444269222cf41c1066c34/TP_4/GUI.cpp
+++ TP_4003ec51a8b444269222cf41c1066c34/TP_4/GUI.cpp
@@ -3,11 +3,12 @@
 using namespace System;
 using namespace System::Windows::Forms;
 
-[STAThread]//leave this as is
+[STAThread]
 void main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	TP_4::GUI gui;
 	Application::Run(%gui);
+	
 }
 
--- TP_4003ec51a8b444269222cf41c1066c34/TP_4/GUI.h
+++ TP_4003ec51a8b444269222cf41c1066c34/TP_4/GUI.h
@@ -37,12 +37,15 @@ namespace TP_4 {
 	private: System::Windows::Forms::Button^  button1;
 	protected:
 	private: System::Windows::Forms::Button^  button2;
+	private: System::IO::Ports::SerialPort^  serialPort1;
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+	private: System::ComponentModel::IContainer^  components;
 
 	private:
 		/// <summary>
 		/// Wymagana zmienna projektanta.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -51,8 +54,11 @@ namespace TP_4 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->serialPort1 = (gcnew System::IO::Ports::SerialPort(this->components));
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -63,28 +69,47 @@ namespace TP_4 {
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &GUI::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(67, 89);
+			this->button2->Location = System::Drawing::Point(67, 148);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"button2";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &GUI::button2_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(165, 95);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(80, 17);
+			this->checkBox1->TabIndex = 2;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
 			// 
 			// GUI
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1041, 625);
+			this->Controls->Add(this->checkBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"GUI";
 			this->Text = L"GUI";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		MessageBox::Show( "usuwam bo mogê ;3", "Hey ;3");
+	}
 	};
 }
