69d79366d55ccf78cdb2566dafce570b96576fe1
SP2FET <sp2fet@gmail.com>
2017-06-17 15:42:32 +0200

    WinAPI instead of Windows Forms

---
+++ TP_4003ec51a8b444269222cf41c1066c34/draw/draw2.cpp
@@ -0,0 +1,373 @@
+// draw.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "draw2.h"
+#include <vector>
+#include <cstdio>
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#define MAX_LOADSTRING 100
+#define TMR_1 1
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+
+
+
+// buttons
+HWND hwndButton;
+HWND hText;
+// sent data
+
+RECT drawArea1 = { 350, 20, 750, 400 };
+
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK		DlgProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	Buttons(HWND, UINT, WPARAM, LPARAM);
+
+void DrawDoubleBuffer(HWND hWnd);
+
+bool CALLBACK SetFont(HWND child, LPARAM font) {
+	SendMessage(child, WM_SETFONT, font, true);
+	return true;
+}
+
+
+void repaintWindow(HWND hWnd, HDC &hdc, PAINTSTRUCT &ps, RECT *drawArea)
+{
+
+	if (drawArea == NULL)
+		InvalidateRect(hWnd, NULL, FALSE); // repaint all
+	else
+		InvalidateRect(hWnd, drawArea, FALSE); //repaint drawArea
+
+	DrawDoubleBuffer(hWnd);
+
+}
+
+
+// main function (exe hInstance)
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+
+	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	
+
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR           gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_DRAW, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DRAW));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	GdiplusShutdown(gdiplusToken);
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DRAW));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_DRAW);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd;
+	hInst = hInstance; // Store instance handle (of exe) in our global variable
+
+	// main window
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, 0, 800, 600, NULL, NULL, hInstance, NULL);
+
+	
+	// create button and store the handle    
+
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("DISCARD SAMPLES"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		100, 190,                                  // the left and top co-ordinates
+		110, 25,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)DISCARD_BTN,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);                               // extra bits you dont really need
+
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zoom IN"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		280, 20,                                  // the left and top co-ordinates
+		60, 25,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_ZOOM_IN,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);                               // extra bits you dont really need
+
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zoom OUT"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		280, 375,                                  // the left and top co-ordinates
+		60, 25,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_ZOOM_OUT,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                     // The class name required is button
+		TEXT("Zoom X IN"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		345, 420,                                  // the left and top co-ordinates
+		60, 25,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_ZOOM_X_IN,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT("Zoom X OUT"),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		682, 420,                                  // the left and top co-ordinates
+		70, 25,                              // width and height
+		hWnd,                                 // parent window handle
+		(HMENU)ID_ZOOM_X_OUT,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);
+	
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	EnumChildWindows(hWnd, (WNDENUMPROC)SetFont, (LPARAM)GetStockObject(DEFAULT_GUI_FONT));
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window (low priority)
+//  WM_DESTROY	- post a quit message and return
+//
+//
+
+void DrawDoubleBuffer(HWND hWnd)
+{
+	RECT Client_Rect;
+	GetClientRect(hWnd, &Client_Rect);
+	int win_width = Client_Rect.right - Client_Rect.left;
+	int win_height = Client_Rect.bottom + Client_Rect.left;
+	PAINTSTRUCT ps;
+	HDC Memhdc;
+	HDC hdc;
+	HBITMAP Membitmap;
+	hdc = BeginPaint(hWnd, &ps);
+	Memhdc = CreateCompatibleDC(hdc);
+	Membitmap = CreateCompatibleBitmap(hdc, win_width, win_height);
+	SelectObject(Memhdc, Membitmap);
+
+	FillRect(Memhdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW));
+	//loggedData->Draw(Memhdc, drawArea1);
+
+	BitBlt(hdc, 0, 0, win_width, win_height, Memhdc, 0, 0, SRCCOPY);
+	DeleteObject(Membitmap);
+	DeleteDC(Memhdc);
+	DeleteDC(hdc);
+	EndPaint(hWnd, &ps);
+}
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	int ret = 0;
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW));
+		repaintWindow(hWnd, hdc, ps, &drawArea1);
+		EndPaint(hWnd, &ps);
+
+		break;
+	case WM_ERASEBKGND:
+		return true;
+		break;
+	case WM_DESTROY:
+		
+		PostQuitMessage(0);
+		break;
+
+	case WM_CREATE:
+		EnumChildWindows(hWnd, (WNDENUMPROC)SetFont, (LPARAM)GetStockObject(DEFAULT_GUI_FONT));
+		break;
+
+	
+	case WM_TIMER:
+		switch (wParam)
+		{
+		case TMR_1:
+			break;
+		}
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	TCHAR buffer[60];
+	
+	switch (Msg)
+	{
+	case WM_COMMAND:
+	{
+		
+		// reakcja na przyciski
+		switch (LOWORD(wParam))
+		{
+		case ID_YES: EndDialog(hwnd, ID_YES); break;
+		case ID_NO: EndDialog(hwnd, ID_NO); break;
+		}
+	}
+	break;
+	
+	default: return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
