f3ba9e165e3ae95cafe732b06cfed995f7b96856
gynvael515 <gynvael515@wp.pl>
2017-06-17 21:46:21 +0200

    Add files via upload

---
+++ c25bd6c6959340a1870801e1a7d97c86/Resource.h
@@ -0,0 +1,31 @@
+//TIMERS
+#define ID_TIMER 1
+#define ID_TIMER_2 2
+//BUTTONS
+#define ID_BUTTON_0 0
+#define ID_BUTTON_1 1
+#define ID_BUTTON_2 2
+#define ID_BUTTON_3 3
+#define ID_BUTTON_4 4
+#define ID_BUTTON_5 5
+#define ID_BUTTON_6 6
+#define ID_BUTTON_7 7
+#define ID_BUTTON_8 8
+#define ID_BUTTON_9 9
+#define ID_BUTTON_10 10
+#define ID_BUTTON_11 11
+//////
+//RADIO BUTTONS
+#define ID_RADIO_1 91
+#define ID_RADIO_2 92
+#define ID_RADIO_3 93
+#define ID_RADIO_4 94
+#define ID_RADIO_5 95
+//DEFINE
+#if defined(UNICODE) && !defined(_UNICODE)
+    #define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+    #define UNICODE
+#endif
+//ext includes
+
