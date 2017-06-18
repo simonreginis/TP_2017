45dcdbb2b5f9cd3f2f42c2e014bae3dff7a8d899
coopper5120 <osiem88888888@onet.pl>
2017-06-18 13:25:25 +0200

    Added communication structure

--- TP_4003ec51a8b444269222cf41c1066c34/draw/elevator.h
+++ TP_4003ec51a8b444269222cf41c1066c34/draw/elevator.h
@@ -4,20 +4,40 @@
 
 using namespace std;
 
-typedef vector<vector<int>> vector2D;
+#define MAX_LOAD 600
+#define HUMAN_WEIGHT 70
+
+typedef vector<vector<int>> vector2D_t;
+struct elev_out_t
+{
+	vector <int> elev_content;
+	int			 next_floor;
+	int			 people_in;
+	int			 people_out;
+	int			 load;
+};
 
 class CElevator
 {
 private:
 	int			elev_pos;
 	vector<int> elev_content;
-	vector2D    floor_array;
+	vector2D_t  floor_array;
 	queue <int> floor_order;
 
 public:
-	void		load_array(vector2D ext_array);
-	void		make_order();
-	queue <int> get_order();
-	void		make_turn();
+	CElevator();
 
+	elev_out_t	make_turn(vector2D_t ext_array);
+	void		load_array(vector2D_t ext_array);
+	bool		check_array(vector2D_t ext_array);
+	void		make_order();
+	int			select_floor();
+
+	elev_out_t  make_elev_out();
+	int		    load_people();
+	int		    unload_people();
+	int			sum_elev_people();
+	int			get_load();
+	bool		check_load();
 };
