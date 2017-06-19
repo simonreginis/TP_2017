999c5402e00760d380a46da4645c4279efb686c6
airpp4 <air460@wp.pl>
2017-06-19 17:41:24 +0200

    Projekt4

---
+++ Projekt4_6fb274ef6eed4f1b98d31d3a2373839f/Resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Projekt4.rc
+//
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_PROJEKT4_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_PROJEKT4			107
+#define IDI_SMALL				108
+#define IDC_PROJEKT4			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+
+#define ID_TIMER_1					200
+#define ID_B_DODAJ					201
+#define ID_B_ANULUJ					202
+#define ID_ROZMIAR				203
+#define ID_WAGA					204
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
