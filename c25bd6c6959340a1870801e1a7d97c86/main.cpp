6c32c175ba5283dfe280df48f9d07505f8547beb
gynvael515 <gynvael515@wp.pl>
2017-06-19 09:23:49 +0200

    Update main.cpp

--- c25bd6c6959340a1870801e1a7d97c86/main.cpp
+++ c25bd6c6959340a1870801e1a7d97c86/main.cpp
@@ -26,3 +26,227 @@ struct elevator
 elevator lift;
 
 std::vector <USI> buttons_on_level;  //// 0-0 1-down 2-up 3-all
+
+class human
+{
+public:
+    human(USI level, USI number);
+    ~human(){;}
+    void wait_for_lift();
+    bool enter_lift();
+    void exit_lift();
+    USI get_pos_x() {return pos_x;}
+    USI get_pos_y() {return pos_y;}
+    void set_pos_y(USI temp) {pos_y=temp;}
+    USI get_side() {return side;}
+    USI set_side(USI side_temp ) {side=side_temp;}
+    USI get_dir() {return direction;}
+    void set_dir(USI dir) {direction=dir;}
+    USI get_level() {return start_level;}
+    USI get_dest() {return dest_level;}
+    USI get_id() {return id;}
+private:
+    USI start_level=0,dest_level=0, pos_x=0,pos_y=0, side=2, direction=0, h_weight=70, id=0; // SIDE=1 - Lewa
+};
+
+void set_max_or_min(USI lvl)
+{
+    if(lvl<lift.current_level)
+            {
+                if(lvl<lift.min_level)
+                    lift.min_level=lvl;
+                if(lift.direction==0)
+                    lift.direction=1;
+            }
+            else if(lvl>lift.current_level)
+            {
+                if(lvl>lift.max_level)
+                    lift.max_level=lvl;
+                if(lift.direction==0)
+                    lift.direction=2;
+            }
+}
+
+human::human(USI level, USI number)
+{
+    id=number;
+    start_level=level;
+    (level%2==0) ? set_side(1) : set_side(2);
+    dest_level=human_destination;
+    (get_side()==1) ? pos_x=10 : pos_x=1014;
+    pos_y=(SHAFT_Y2-17 -((start_level)*lift.height));
+    if(dest_level>start_level)
+    {
+        set_dir(2); //UP
+        if(buttons_on_level[start_level]==0 || buttons_on_level[start_level]==2 || buttons_on_level[start_level]==4) //DO FUNCKJI????
+            buttons_on_level[start_level]=2;
+        else if(buttons_on_level[start_level]==1 || buttons_on_level[start_level]==3)
+            buttons_on_level[start_level]=3;
+    }
+    else
+    {
+        set_dir(1); //DOWN
+        if(buttons_on_level[start_level]==0 || buttons_on_level[start_level]==1 || buttons_on_level[start_level]==4) //DO FUNCKJI????
+            buttons_on_level[start_level]=1;
+        else if(buttons_on_level[start_level]==2 || buttons_on_level[start_level]==3)
+            buttons_on_level[start_level]=3;
+    }
+}
+
+void human::wait_for_lift()
+{
+    HDC hdcTemp = GetDC( hwnd );
+    if(get_side()==1)
+    {
+        //if(pos_x<(SHAFT_X1-10))
+        if(GetPixel(hdcTemp, pos_x+32, pos_y)==RGB(255,255,255))
+            pos_x++;
+        else
+            {
+            set_max_or_min(start_level);
+            set_max_or_min(dest_level);
+            }
+    }
+    else if(get_side()==2)
+        {
+        //if(pos_x>(SHAFT_X2+10))
+        if(GetPixel(hdcTemp, pos_x-32, pos_y)==RGB(255,255,255))
+            pos_x--;
+        else
+            {
+            set_max_or_min(start_level);
+            set_max_or_min(dest_level);
+            }
+        }
+    DeleteDC( hdcTemp );
+}
+
+bool human::enter_lift()        //!!!!BUG OPEN DOOR (DIR SIE ZMIENIA)
+{
+    if(lift.curr_weight+h_weight<lift.max_weight)
+        {
+            if(get_side()==1)
+                {
+                if(pos_x<=lift.last_place_x+32)
+                    pos_x++;
+                else
+                    {
+                    lift.last_place_x+=32;
+                    lift.curr_weight+=h_weight;             //!!!!!! FUNKCJAAAAAAAA
+                    set_side(0);
+                    if(buttons_on_level[dest_level]==0)
+                        buttons_on_level[dest_level]=4;
+                    }
+                }
+            else if(get_side()==2)
+                {
+                if(pos_x>=lift.last_place_x+32)
+                    pos_x--;
+                else
+                    {
+                    lift.last_place_x+=32;
+                    lift.curr_weight+=h_weight;
+                    set_side(0);
+                    if(buttons_on_level[dest_level]==0)
+                        buttons_on_level[dest_level]=4;
+                    }
+                }
+            return true;
+        }
+    return  false;      //IF WEIGHT > LIMIT
+}
+
+void human::exit_lift()
+{
+    if(dest_level%2==0){
+        if(pos_x>=30)
+            pos_x--;
+        else
+        {
+            set_side(5); //5- is going out
+            lift.last_place_x-=32;
+            lift.curr_weight-=70;
+        }
+    }
+    else if(dest_level%2==1)
+        if(pos_x<=994)
+            pos_x++;
+        else
+        {
+            set_side(5); //5- is going out
+            lift.last_place_x-=32;
+            lift.curr_weight-=70;
+        }
+}
+
+std::vector <human> people;
+
+void delete_human(USI id)
+{
+    people[id]=people.back();
+    people.back().~human(); //! ????
+    people.pop_back();
+}
+
+bool humans_exit_lift(HDC hdcBufor)            //TRUE WHEN ALL IN  / WEIGHT > LIMIT!, FALSE WHEN LIFT IS NOT MOVING
+{
+    bool everyone_out=true, delete_this_human=false;
+    USI delete_id=0;
+    for (std::vector<human>::iterator it = people.begin() ; it != people.end(); ++it)
+        {
+            if(it->get_dest()==lift.current_level && it->get_side()==0)   //! co gdy winda wjedzie na max floor i bedzie miala zjechac w dol
+            {
+                it->exit_lift();
+                if(it->get_side()!=5)
+                    everyone_out=false;
+                else
+                    {
+                    delete_this_human=true;
+                    delete_id=it->get_id();
+                    }
+            }
+        }
+    if(delete_this_human)
+        delete_human(delete_id);
+    if(everyone_out==true)
+    {
+        lift.current_state=3;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool humans_enter_lift(HDC hdcBufor)            //TRUE WHEN ALL IN  / WEIGHT > LIMIT!, FALSE WHEN LIFT IS NOT MOVING
+{
+    bool everyone_in=true;
+    for (std::vector<human>::iterator it = people.begin() ; it != people.end(); ++it)
+        {
+            if(it->get_level()==lift.current_level && (it->get_dir()==lift.direction || lift.current_level==lift.max_level || lift.current_level==lift.min_level)) //! co gdy winda wjedzie na max floor i bedzie miala zjechac w dol
+            {
+                if(!it->enter_lift())
+                    {
+                        lift.current_state=4;
+                        return true;
+                    }
+                if(it->get_side()!=0)
+                    everyone_in=false;
+            }
+        }
+    if(everyone_in==true)
+    {
+        if(buttons_on_level[lift.current_level]==3)
+        {
+            if(lift.direction==2)
+                buttons_on_level[lift.current_level]=1;
+            else
+                buttons_on_level[lift.current_level]=2;
+        }
+        else
+            buttons_on_level[lift.current_level]=0;
+        lift.current_state=4;
+        return true;
+    }
+    else
+        return false;
+}
